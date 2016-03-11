9b586724f50c215a7e719c46cde2e5c3fecd3f02
Ari-Pekka Koponen <ap.koponen@gmail.com>
2016-03-11 14:38:04 +0200

    Force POST-parameters on token endpoint.

--- lib/class-wp-rest-oauth2-token-controller.php
+++ lib/class-wp-rest-oauth2-token-controller.php
@@ -12,8 +12,9 @@ class WP_REST_OAuth2_Token_Controller extends WP_REST_OAuth2_Server {
 	$required_params = array( 'client_id', 'client_secret', 'grant_type', 'redirect_uri' );
 
 	$required_missing = false;
+	$request_body_params = $request->get_body_params();
 	foreach ( $required_params as $required_param ) {
-	  if ( empty( $request->get_param( $required_param ) ) ) {
+	  if ( empty( $request_body_params[ $required_param ] ) ) {
 		$required_missing = true;
 	  }
 	}
@@ -26,7 +27,7 @@ class WP_REST_OAuth2_Token_Controller extends WP_REST_OAuth2_Server {
 
 	// Check that client credentials are valid
 	// We may be able to move this up in the first check as well
-	if ( !WP_REST_OAuth2_Storage_Controller::authenticateClient( $request[ 'client_id' ], $request[ 'client_secret' ] ) ) {
+	if ( !WP_REST_OAuth2_Storage_Controller::authenticateClient( $request_body_params[ 'client_id' ], $request_body_params[ 'client_secret' ] ) ) {
 	  $error = WP_REST_OAuth2_Error_Helper::get_error( 'invalid_request' );
 
 	  return new WP_REST_OAuth2_Response_Controller( $error );
@@ -34,12 +35,12 @@ class WP_REST_OAuth2_Token_Controller extends WP_REST_OAuth2_Server {
 
 	$supported_grant_types = apply_filters( 'wp_rest_oauth2_grant_types', array( 'authorization_code' ) );
 
-	if ( !in_array( $request[ 'grant_type' ], $supported_grant_types ) ) {
+	if ( !in_array( $request_body_params[ 'grant_type' ], $supported_grant_types ) ) {
 	  $error = WP_REST_OAuth2_Error_Helper::get_error( 'unsupported_grant_type' );
 	  return new WP_REST_OAuth2_Response_Controller( $error );
 	}
 
-	$code = WP_REST_OAuth2_Authorization_Code::get_authorization_code( $request[ 'code' ] );
+	$code = WP_REST_OAuth2_Authorization_Code::get_authorization_code( $request_body_params[ 'code' ] );
 
 	// Authorization code MUST exists
 	if ( empty( $code ) ) {
@@ -49,8 +50,8 @@ class WP_REST_OAuth2_Token_Controller extends WP_REST_OAuth2_Server {
 	}
 
 	// Authorization code redirect URI and client MUST match, and the code should not have expired
-	$is_valid_redirect_uri = !empty( $code[ 'redirect_uri' ] ) && $code[ 'redirect_uri' ] === $request[ 'redirect_uri' ];
-	$is_valid_client = $code[ 'client_id' ] === $request[ 'client_id' ];
+	$is_valid_redirect_uri = !empty( $code[ 'redirect_uri' ] ) && $code[ 'redirect_uri' ] === $request_body_params[ 'redirect_uri' ];
+	$is_valid_client = $code[ 'client_id' ] === $request_body_params[ 'client_id' ];
 	$code_has_expired = $code[ 'expires' ] < time();
 	if ( !$is_valid_redirect_uri || !$is_valid_client || $code_has_expired ) {
 	  $error = WP_REST_OAuth2_Error_Helper::get_error( 'invalid_request' );
