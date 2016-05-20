99c87206b5e7ae1f6d18ed0d910cb6aa3e637974
Ari-Pekka Koponen <ap.koponen@gmail.com>
2016-05-20 18:51:20 +0300

    Fix test mode check.

--- lib/class-wp-rest-oauth2-authorize-controller.php
+++ lib/class-wp-rest-oauth2-authorize-controller.php
@@ -14,7 +14,7 @@ class WP_REST_OAuth2_Authorize_Controller extends WP_REST_OAuth2_Server {
 
   // Validate Request
   static function validate ( WP_REST_Request $request ) {
-	if ( !is_ssl() AND ( defined( 'WP_REST_OAUTH2_TEST_MODE' ) && !WP_REST_OAUTH2_TEST_MODE ) ) {
+	if ( !is_ssl() && ( !defined( 'WP_REST_OAUTH2_TEST_MODE' ) || !WP_REST_OAUTH2_TEST_MODE ) ) {
 	  return new WP_Error( 'SSL is required' );
 	}
 
